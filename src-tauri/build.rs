31f7416185da25cf6f7b0ebee3d0f60a1aac1eb7
awhittle6 <awhittlex2@gmail.com>
2025-03-28 11:26:06 -0700

    update protos

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,6 @@
 fn main() {
+    tonic_build::configure().out_dir("./protos")
+    .compile_protos(&["protos/chat.proto"], &[""])
+    .unwrap_or_else(|e| eprintln!("Error here: {e:?}"));
     tauri_build::build()
 }
